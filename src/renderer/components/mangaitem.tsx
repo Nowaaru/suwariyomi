19918093c746b3d26151bdf97684675563a2158b
Nowaaru <zackyboy135@gmail.com>
2022-01-09 21:23:16 -0700

    implement tag integration, fix page formatting

--- src/renderer/components/mangaitem.tsx
+++ src/renderer/components/mangaitem.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, css } from 'aphrodite';
+import Tag from './tag';
 
 type MangaItemListProps = {
   displayType: 'list';
@@ -27,37 +28,72 @@ type MangaItemProps = MangaItemGenericProps &
 
 const styles = StyleSheet.create({
   mangaItemListContainer: {
-    display: 'inline-flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    alignItems: 'center',
     marginBottom: '10px',
-    width: '100%',
+    width: 'calc(100% - 20px)',
     maxHeight: '300px',
+    backgroundColor: '#0c0a0c',
+    verticalAlign: 'top',
   },
 
   mangaItemListCover: {
+    margin: '10px 15px 10px 10px',
+    verticalAlign: 'top',
+    display: 'inline-flex',
     width: 'fit-content',
     height: 'fit-content',
-    maxWidth: '25%',
-    maxHeight: '100%',
   },
 
   mangaItemListCoverImage: {
-    width: '256px',
     height: '256px',
     objectFit: 'contain',
   },
 
   mangaItemTitle: {
-    fontSize: '1.5em',
+    fontSize: '2em',
     fontWeight: 'bold',
+    margin: '0',
+    marginTop: '20px',
     marginBottom: '10px',
     color: '#fff',
+    fontFamily: '"PT Sans Narrow", "Roboto", "Helvetica", "Arial", sans-serif',
+  },
+  mangaItemSynopsis: {
+    display: 'block',
+    fontSize: '1em',
+    color: '#fff',
+    fontFamily: '"Poppins", "Roboto", "Helvetica", "Arial", sans-serif',
+    width: '100%',
+    height: '85%',
+    maxWidth: '600px',
+  },
+  mangaMetadata: {
+    verticalAlign: 'top',
+    display: 'inline-block',
+    width: '65%',
+    height: '100%',
+  },
+
+  mangaItemInformationMain: {
+    display: 'inline-block',
+    width: '100%',
+    height: 'fit-content',
+  },
+
+  // Manga item tags is a horizontal list of tags.
+  mangaItemTags: {
+    display: 'inline-block',
+    width: '100%',
+    height: 'fit-content',
+    margin: '0px 0px 10px 0px',
+    verticalAlign: 'top',
   },
 });
 
 const mangaItem = (props: MangaItemProps) => {
+  const mangaTags = props.tags.map((tag) => (
+    <Tag key={tag} name={tag} type="normal" />
+  ));
+
   switch (props.displayType) {
     case 'list':
       return (
@@ -69,8 +105,14 @@ const mangaItem = (props: MangaItemProps) => {
               alt={props.title}
             />
           </div>
-          <div>
-            <h3>{props.title}</h3>
+          <div className={css(styles.mangaMetadata)}>
+            <div className={css(styles.mangaItemInformationMain)}>
+              <h3 className={css(styles.mangaItemTitle)}>{props.title}</h3>
+              <div className={css(styles.mangaItemTags)}>{mangaTags}</div>
+              <span className={css(styles.mangaItemSynopsis)}>
+                {props.synopsis}
+              </span>
+            </div>
           </div>
         </div>
       );
