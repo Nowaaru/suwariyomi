bf781f12daa050beca969b46a9663fb0700fc3ff
Nowaaru <zackyboy135@gmail.com>
2022-01-11 01:12:56 -0700

    routing

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,13 +2,16 @@ import './components/menu';
 import './css/App.css';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 // eslint-disable-next-line import/no-named-default
-import { default as Main } from './components/library';
+import Library from './components/library';
+import Login from './components/login';
 
+const { checkAuthenticated } = window.electron.auth;
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Library />} />
       </Routes>
     </Router>
   );
