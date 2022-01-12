f4ae7a9602119053e599599501c82a7320558fa7
Nowaaru <zackyboy135@gmail.com>
2022-01-12 16:05:52 -0700

    Search utility

---
+++ src/renderer/util/search.ts
@@ -0,0 +1,30 @@
+const parseQuery = (searchQuery: string) => {
+  const splitQuery = searchQuery.split(' ');
+  const parsedQueries: string[] = [];
+  const quotationStore: string[] = [];
+
+  splitQuery.forEach((query: string) => {
+    if (query.startsWith('"') && !query.endsWith('"')) {
+      quotationStore.push(query.slice(1));
+      return;
+    }
+
+    if (quotationStore.length) {
+      if (query.endsWith('"')) {
+        quotationStore.push(query.slice(0, -1));
+        parsedQueries.push(quotationStore.join(' '));
+        // reset quotation store
+        while (quotationStore.length) quotationStore.pop();
+        return;
+      }
+
+      quotationStore.push(query);
+    }
+    parsedQueries.push(query);
+  });
+
+  if (quotationStore.length) return false;
+  return parsedQueries;
+};
+
+export default parseQuery;
