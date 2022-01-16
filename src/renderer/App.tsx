f570307f574521378790245cde443e80471c362a
Nowaaru <zackyboy135@gmail.com>
2022-01-16 03:46:49 -0700

    routing

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,16 +1,19 @@
-import './components/menu';
 import './css/App.css';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 // eslint-disable-next-line import/no-named-default
 import Library from './pages/library';
 import Login from './pages/login';
+import Search from './pages/search';
+import NotFound from './pages/404';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Library />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/library" element={<Library />} />
       </Routes>
     </Router>
   );
