b9815fb93b6725bbd2646685b98d72be58371453
Nowaaru <zackyboy135@gmail.com>
2022-01-18 04:00:19 -0700

    types

--- src/sources/static/base.ts
+++ src/sources/static/base.ts
@@ -1,11 +1,39 @@
 import { Manga, Chapter } from '../../main/util/dbUtil';
 
+export type SearchFilters = {
+  query: string;
+  results?: number;
+  offset?: number;
+};
+
+type SearchFilterFieldBase = {
+  writeTo: string;
+};
+
+type SearchFilterFieldTypeAutoComplete = {
+  type: 'autocomplete';
+  options: string[];
+};
+
+type SearchFilterFieldTypeCheckbox = {
+  type: 'checkbox';
+  checked?: boolean;
+  display: string;
+};
+
+export type SearchFilterFieldTypes = {
+  [categoryName: string]: (SearchFilterFieldBase &
+    (SearchFilterFieldTypeAutoComplete | SearchFilterFieldTypeCheckbox))[];
+};
+
 export default abstract class SourceBase {
   protected abstract _sourceName: string;
 
   public abstract getName(): typeof SourceBase.prototype._sourceName;
 
   protected abstract searchFilters: any;
+
+  protected abstract searchFilterFieldTypes: SearchFilterFieldTypes;
 
   public abstract setFilters(
     searchFilters: typeof SourceBase.prototype.searchFilters
