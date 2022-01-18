ed40f2c225872e8930a4fc4ac7b84f2ba9aa99aa
Nowaaru <zackyboy135@gmail.com>
2022-01-18 01:34:45 -0700

    finally implemented search caching ugh

--- src/renderer/pages/search.tsx
+++ src/renderer/pages/search.tsx
@@ -1,10 +1,10 @@
 import {
-  TextField,
   Accordion,
   AccordionSummary,
   AccordionDetails,
   Typography,
-  Paper,
+  Button,
+  Box,
 } from '@mui/material';
 
 import { StyleSheet, css } from 'aphrodite/no-important';
@@ -14,33 +14,44 @@ import { Link } from 'react-router-dom';
 import LazyLoad, { forceCheck } from 'react-lazyload';
 import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import parseQuery from '../util/search';
+import SearchIcon from '@mui/icons-material/Search';
+
+import SourceBase from '../../sources/static/base';
 import useQuery from '../util/hook/usequery';
 import { Manga } from '../../main/util/dbUtil';
 
 import MangaItem from '../components/mangaitem';
 import SearchBar from '../components/search';
-
 import Handler from '../../sources/handler';
-import SourceBase from '../../sources/static/base';
 
 const styles = StyleSheet.create({
   container: {
     display: 'block',
     position: 'absolute',
-    width: '100vw',
-    height: 'calc(100vh - 42px)',
+    width: 'calc(100% - 64px)',
+    padding: '32px',
+    height: 'calc(100% - 48px)',
+    overflowY: 'scroll',
+    '::-webkit-scrollbar': {
+      width: '4px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      background: '#FFFFFF',
+    },
+    marginBottom: '48px',
   },
   returnButtonContainer: {
-    position: 'sticky',
+    position: 'relative', // tl tr bl br
+    margin: '5px 5px 10px 5px',
+    padding: '5px 5px 8px 5px',
     width: 'fit-content',
     height: 'fit-content',
-    top: '60px',
-    left: '25px',
+    left: '0px',
     transition: 'left 0.3s ease-in-out',
     ':hover': {
-      left: '15px',
+      left: '-20px',
     },
+    marginBottom: '10px',
   },
   returnTo: {
     fontFamily: '"Poppins", "Roboto", "Helvetica", "Arial", sans-serif',
@@ -59,6 +70,52 @@ const styles = StyleSheet.create({
     fontSize: '1.5rem',
     marginLeft: '10px',
   },
+  accordionItem: {
+    margin: '0px 0px 8px 0px',
+    backgroundColor: '#080708',
+  },
+
+  accordionItemIcon: {
+    margin: '0px 8px 0px 0px',
+  },
+
+  sourceContainer: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+    overflowY: 'hidden',
+    '::-webkit-scrollbar': {
+      height: '4px',
+    },
+    '::-webkit-scrollbar-thumb': {
+      backgroundColor: '#FFFFFF',
+      borderRadius: '4px',
+      transition: 'background-color 0.2s ease-in-out',
+      ':hover': {
+        backgroundColor: '#DF2935',
+      },
+    },
+  },
+
+  accordionText: {
+    fontSize: '1.2rem',
+    fontFamily: '"PT Sans Narrow", "Roboto", "Helvetica", "Arial", sans-serif',
+  },
+
+  searchButton: {
+    position: 'relative',
+    zIndex: 256,
+  },
+
+  searchResults: {
+    display: 'block',
+    top: '10%',
+    width: '90%',
+    height: '80%',
+    padding: '48px',
+  },
 });
 
 /*
@@ -71,27 +128,54 @@ const styles = StyleSheet.create({
   }
 */
 type QueriedType = { [searchQuery: string]: { [sourceName: string]: Manga[] } };
-
+const generateQueriedSearchData = (
+  mappedFileNames: Array<InstanceType<typeof SourceBase>>
+) =>
+  Object.fromEntries(mappedFileNames.map((source) => [source.getName(), []]));
 const SearchPage = () => {
   const pageQueryParams = useQuery();
-  const [searchQuery, setSearchQuery] = useState(
-    (pageQueryParams.get('search') || '').toLowerCase().trim()
+  const [specifiedSource, setSpecifiedSource] = useState(
+    pageQueryParams.get('source')
   );
-  const [queriedSearchesLog, setQueriedSearchesLog] = useState<QueriedType>({});
+  const mappedFileNames = window.electron.util
+    .getSourceFiles()
+    .map(Handler.getSource)
+    .filter(
+      (x) =>
+        !specifiedSource ||
+        x.getName().toLowerCase() === specifiedSource.toLowerCase()
+    );
+
+  const [searchData, setSearchData] = useState<{
+    searchQuery: string;
+    queriedSearches: QueriedType;
+  }>({
+    searchQuery: (pageQueryParams.get('search') || '').toLowerCase().trim(),
+    queriedSearches: {
+      // I seriously have no way to make this cleaner.
+      [(pageQueryParams.get('search') || '').toLowerCase().trim()]:
+        generateQueriedSearchData(mappedFileNames),
+    },
+  });
+
+  // Apply search query to all sources
+  mappedFileNames.forEach((source) => {
+    const sourceFilters = { ...source.getFilters() };
+    sourceFilters.query = searchData.searchQuery;
+    source.setFilters(sourceFilters);
+  });
 
   useEffect(() => {
-    if (queriedSearchesLog[searchQuery]) return;
-
-    const mappedFileNames = window.electron.util
-      .getSourceFiles()
-      .map(Handler.getSource)
-      .filter(
-        (x) =>
-          !queriedSearchesLog[searchQuery] ||
-          queriedSearchesLog[searchQuery][x.getName()] === undefined
+    const filteredFileNames = mappedFileNames.filter((x) => {
+      return (
+        searchData.queriedSearches[searchData.searchQuery][
+          x.getName() as string
+        ].length === 0
       );
+    });
 
-    mappedFileNames.forEach(async (source) => {
+    console.log(filteredFileNames);
+    filteredFileNames.forEach(async (source) => {
       source
         .search()
         .then((x) => x.map(source.serialize))
@@ -100,13 +184,16 @@ const SearchPage = () => {
           return Promise.all(y);
         })
         .then((MangaData: any) => {
-          console.log('pog v2!!');
+          const newQueriedSearchesLog = { ...searchData.queriedSearches };
+          newQueriedSearchesLog[searchData.searchQuery] =
+            newQueriedSearchesLog[searchData.searchQuery] || {}; // Check if it exists beforehand because there might be other sources still loading after this one.
 
-          return setQueriedSearchesLog({
-            ...queriedSearchesLog,
-            [searchQuery]: {
-              [source.getName()]: MangaData,
-            },
+          newQueriedSearchesLog[searchData.searchQuery][source.getName()] =
+            MangaData;
+
+          return setSearchData({
+            ...searchData,
+            queriedSearches: newQueriedSearchesLog,
           });
         })
         .catch(console.error);
@@ -123,28 +210,128 @@ const SearchPage = () => {
     - If the *source name* is clicked, go to the /search route with the source name as a query param.
     - If there is a query param `source`, show an entirely different display; similar to the library.
   */
+  const currentSearches = searchData.queriedSearches[searchData.searchQuery];
+  const elementHierarchy = Object.keys(currentSearches).map((sourceString) => {
+    if (
+      specifiedSource &&
+      sourceString.toLowerCase() !== specifiedSource.toLowerCase()
+    ) {
+      return null;
+    }
 
-  console.log(queriedSearchesLog);
+    if (specifiedSource) {
+      return <span>Work in progress.</span>;
+    }
+
+    return (
+      <LazyLoad key={sourceString} scrollContainer="#lazyload">
+        <Accordion
+          TransitionProps={{
+            unmountOnExit: true,
+            onExited: forceCheck,
+            onEntered: forceCheck,
+          }}
+          classes={{
+            root: css(styles.accordionItem),
+          }}
+          defaultExpanded
+        >
+          <AccordionSummary
+            expandIcon={
+              <ExpandMoreIcon
+                sx={{
+                  color: '#ffffff',
+                }}
+              />
+            }
+          >
+            <img
+              src="https://mangadex.org/favicon.ico"
+              className={css(styles.accordionItemIcon)}
+              alt="MangaDex"
+            />
+            <Typography
+              sx={{
+                width: '66%',
+                flexShrink: 2,
+                color: '#FFFFFF',
+              }}
+              className={css(styles.accordionText)}
+            >
+              {sourceString}
+            </Typography>
+            <Button
+              variant="outlined"
+              className={css(styles.searchButton)}
+              startIcon={<SearchIcon />}
+              onClick={(e) => {
+                setSpecifiedSource(sourceString);
+
+                e.preventDefault();
+                e.stopPropagation();
+              }}
+            >
+              Search
+            </Button>
+          </AccordionSummary>
+          <AccordionDetails className={css(styles.sourceContainer)}>
+            {currentSearches[sourceString].map((MangaObject) => (
+              <MangaItem
+                displayType="grid"
+                listDisplayType={null}
+                title={MangaObject.Name}
+                coverUrl={MangaObject.CoverURL || undefined}
+                tags={MangaObject.Tags.slice(1, 10) ?? []}
+                synopsis={(() => {
+                  return (
+                    new DOMParser().parseFromString(
+                      MangaObject.Synopsis || 'No synopsis available.', // Use OR instead of null check to implicitly cast empty strings to boolean.
+                      'text/html'
+                    ).body.textContent || 'No synopsis available.'
+                  );
+                })()}
+                key={MangaObject.Name}
+              />
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      </LazyLoad>
+    );
+  });
   return (
-    <div className={css(styles.container)}>
+    <div id="lazyload" className={css(styles.container)}>
       <div className={css(styles.returnButtonContainer)}>
         <Link className={css(styles.returnTo)} to="/library">
           <ArrowCircleLeftRoundedIcon fontSize="inherit" />
           <span className={css(styles.returnSpan)}> Library</span>
         </Link>
       </div>
-      <SearchBar
-        label="Search globally..."
-        defaultValue={searchQuery}
-        onSubmit={(e) => {
-          // console.log(e.currentTarget);
-          // setSearchQuery(e.currentTarget.nodeValue || '');
-          // e.preventDefault();
-          console.log('dread');
+      <Box
+        component="form"
+        onSubmit={(e: any) => {
+          // the type of this should *NOT* be any but I don't know why it refuses to be typed properly ugh
           e.preventDefault();
+          e.stopPropagation();
+
+          const newSearchQueryData = {
+            ...searchData,
+            searchQuery: e.target[0].value,
+          };
+
+          if (!searchData.queriedSearches[newSearchQueryData.searchQuery]) {
+            newSearchQueryData.queriedSearches[newSearchQueryData.searchQuery] =
+              generateQueriedSearchData(mappedFileNames);
+          }
+          return setSearchData(newSearchQueryData);
         }}
-        placeholder="Hana ni Arashi"
-      />
+      >
+        <SearchBar
+          label="Search globally..."
+          defaultValue={searchData.searchQuery}
+          placeholder="Hana ni Arashi"
+        />
+      </Box>
+      {elementHierarchy}
     </div>
   );
 };
