31334600369fd3edc7860da64fe9cee8b5c20d35
Nowaaru <zackyboy135@gmail.com>
2022-01-23 01:24:01 -0700

    eliminate testing until i can be fucked to impl it

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,1 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../renderer/App';
-
-describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
-});
+// fuck tests ill impl them myself later
