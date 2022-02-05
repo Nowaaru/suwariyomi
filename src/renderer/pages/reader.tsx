b4f2f60050b24d73ee486528296403b91fbb8aed
Nowaaru <zackyboy135@gmail.com>
2022-02-04 22:22:33 -0700

    Implement proper theming for modal

--- src/renderer/pages/reader.tsx
+++ src/renderer/pages/reader.tsx
@@ -1,13 +1,22 @@
-import { StyleSheet, css } from 'aphrodite';
-import { useRef } from 'react';
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Button,
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { StyleSheet, css, StyleDeclaration, CSSProperties } from 'aphrodite';
 import { URLSearchParams } from 'url';
+import { useRef } from 'react';
 
 import Handler from '../../sources/handler';
 import Sidebar from '../components/sidebar';
 import useQuery from '../util/hook/usequery';
 import SourceBase from '../../sources/static/base';
 
-const Style = StyleSheet.create({
+const stylesObject = {
   container: {
     display: 'block',
     position: 'absolute',
@@ -22,10 +31,52 @@ const Style = StyleSheet.create({
     height: '100%',
     backgroundColor: '#312F2F',
   },
-});
+
+  dialogContainer: {
+    backgroundColor: 'transparent',
+  },
+
+  dialogContainerInner: {
+    backgroundColor: '#312F2F',
+  },
+
+  dialog: {
+    backgroundColor: 'transparent',
+  },
+
+  dialogText: {
+    backgroundColor: 'transparent',
+    color: 'white',
+  },
+
+  dialogActions: {
+    backgroundColor: 'transparent',
+  },
+
+  dialogButton: {
+    backgroundColor: 'transparent',
+    fontWeight: 'bold',
+    color: '#DF2935',
+  },
+
+  dialogTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+  },
+};
+
+const styles = StyleSheet.create<typeof stylesObject>(
+  stylesObject as StyleDeclaration<typeof stylesObject>
+);
 
 const Reader = () => {
   const queryParameters = useQuery();
+  const Navigate = useNavigate();
+
+  const goBack = () => {
+    Navigate('/library');
+  };
+
   const {
     source: sourceId = 'MangaDex',
     id: mangaId = '',
@@ -49,8 +100,35 @@ const Reader = () => {
     `chapterId: ${chapterId} || mangaId: ${mangaId} || pageNumber: ${pageNumber}`
   );
 
+  // No selected source dialog
+  // This is here in case a bug occurs OR a user tries to access a source that doesn't exist.
+  // typically via a direct link / protocol.
+  if (selectedSource)
+    return (
+      <Dialog
+        className={css(styles.dialogContainer)}
+        sx={{
+          '& .MuiDialog-paper': stylesObject.dialogContainerInner,
+        }}
+        open
+        onClose={goBack}
+      >
+        <DialogTitle className={css(styles.dialogTitle)}>Error</DialogTitle>
+        <DialogContent className={css(styles.dialog)}>
+          <DialogContentText className={css(styles.dialogText)}>
+            <span>{`You do not have any sources that go by the name of ${sourceId}.`}</span>
+          </DialogContentText>
+          <DialogActions className={css(styles.dialogActions)}>
+            <Button onClick={goBack} className={css(styles.dialogButton)}>
+              Go Home
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+    );
+
   return (
-    <div className={css(Style.container)}>
+    <div className={css(styles.container)}>
       <Sidebar outOf={32} />
     </div>
   );
