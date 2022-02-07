0a72228c52ca527482a7e667786ce825eaf45b4b
Nowaaru <zackyboy135@gmail.com>
2022-02-07 06:13:35 -0700

    Better sorting

--- src/renderer/util/func.ts
+++ src/renderer/util/func.ts
@@ -1,35 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 import { Chapter } from '../../main/util/manga';
 
-export const getChapterValue = (chapter: Chapter): number => {
-  const numberifiedChapter = Number(chapter.Chapter);
-  const numberifiedChapterVolume = Number(chapter.Volume);
-
-  const isChapterNumber = !Number.isNaN(numberifiedChapter);
-  const isChapterVolumeNumber = !Number.isNaN(numberifiedChapterVolume);
-
-  if (isChapterNumber && isChapterVolumeNumber) {
-    return (
-      numberifiedChapter * (numberifiedChapterVolume + 1) // Add +1 to the volume number in case the volume is weird (i.e., it's 0)
-    );
-  }
-  return isChapterNumber
-    ? numberifiedChapter
-    : isChapterVolumeNumber
-    ? numberifiedChapterVolume
-    : 0;
-};
-
 export const sortChapters = (chapters: Chapter[], isDescending = true) =>
   chapters.sort((a, b) => {
-    const [calculatedA, calculatedB] = [getChapterValue(a), getChapterValue(b)];
-    if (calculatedA === calculatedB)
+    // TODO: Remove repitition
+    if (a.Chapter === b.Chapter)
       // Sort by date instead
       return (
-        new Date(a.PublishedAt).getTime() - new Date(b.PublishedAt).getTime()
+        (isDescending ? -1 : 1) *
+        (new Date(a.PublishedAt).getTime() - new Date(b.PublishedAt).getTime())
       );
 
-    return (isDescending ? -1 : 1) * (calculatedA - calculatedB);
+    return (isDescending ? -1 : 1) * (a.Chapter - b.Chapter);
   });
 
 export const filterChaptersToLanguage = (
