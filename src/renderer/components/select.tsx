104f0ac940f61013a816d44151ef1d10105cbce4
Nowaaru <zackyboy135@gmail.com>
2022-02-17 03:33:09 -0700

    implement utility components

---
+++ src/renderer/components/select.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/destructuring-assignment */
+import { Typography, Select as MaterialSelect, MenuItem } from '@mui/material';
+import type { SelectProps as MaterialSelectProps } from '@mui/material/Select';
+
+import { omit } from 'lodash';
+import { StyleSheet, css } from 'aphrodite';
+
+const stylesObject = {
+  selected: {
+    color: 'white',
+  },
+
+  legend: {
+    opacity: 0,
+    transition: 'opacity 0.2s ease-in-out 0.25s',
+    visibility: 'hidden',
+  },
+
+  selectedLegend: {
+    opacity: 1,
+    position: 'relative',
+    top: '-5px',
+    visibility: 'visible',
+    color: 'white',
+  },
+
+  icon: {
+    color: 'white',
+  },
+
+  iconSelected: {
+    color: '#DF2935',
+  },
+
+  focused: {
+    borderColor: '#DF2935',
+  },
+};
+
+// @ts-ignore Aphrodite Sucks: Part 2
+const styles = StyleSheet.create(stylesObject);
+
+const Select = (
+  props: Exclude<Exclude<MaterialSelectProps, 'children'>, 'renderValue'> & {
+    value: string;
+    values: {
+      // OptionValue: OptionLabel
+      [optionValue: string]: string;
+    };
+  }
+) => {
+  const { value, values, sx } = props;
+  if (!Object.keys(values).find((optionValue) => optionValue === value))
+    throw new Error(`Value "${value}" not found in values`);
+
+  return (
+    <MaterialSelect
+      {...omit(props, 'children', 'renderValue', 'values', 'sx')}
+      sx={{
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': stylesObject.focused,
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline legend span': {
+          ...stylesObject.legend,
+          ...stylesObject.selectedLegend,
+        },
+        '&:not(Mui-focused) svg.MuiSvgIcon-root': {
+          ...stylesObject.icon,
+        },
+        '&.Mui-focused svg.MuiSvgIcon-root': {
+          ...stylesObject.iconSelected,
+        },
+        '&:not(.Mui-focused) .MuiOutlinedInput-notchedOutline legend span':
+          stylesObject.legend,
+        ...(sx ?? {}),
+      }}
+      renderValue={(selected) => {
+        const displayValue = values[selected as string];
+        return (
+          <Typography className={css(styles.selected)}>
+            {displayValue}
+          </Typography>
+        );
+      }}
+    >
+      {Object.keys(values).map((valuesIndex) => (
+        <MenuItem key={valuesIndex} value={valuesIndex}>
+          {values[valuesIndex]}
+        </MenuItem>
+      ))}
+    </MaterialSelect>
+  );
+};
+
+export default Select;
---
+++ src/renderer/components/switch.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import {
+  Switch as MaterialSwitch,
+  SwitchProps as MaterialSwitchProps,
+  Tooltip,
+} from '@mui/material';
+import { omit } from 'lodash';
+import { StyleSheet, css } from 'aphrodite';
+
+const stylesObject = {
+  switchTrackOn: {
+    opacity: 0.6,
+    backgroundColor: '#DF2935',
+  },
+
+  switchTrackOff: {
+    opacity: 0.6,
+    backgroundColor: '#FFFFFF',
+  },
+
+  switchBase: {
+    color: 'white',
+  },
+
+  switchThumb: {},
+
+  switchThumbOn: {
+    color: 'white',
+    backgroundColor: '#DF2935',
+  },
+
+  switchHoverOn: {
+    backgroundColor: '#DF293522',
+  },
+
+  switchHover: {
+    backgroundColor: '#FFFFFF11',
+  },
+};
+
+const Switch = (
+  props: MaterialSwitchProps & Pick<Required<MaterialSwitchProps>, 'checked'>
+) => {
+  const { checked, onChange } = props;
+  return (
+    <Tooltip title={checked ? 'On' : 'Off'}>
+      <MaterialSwitch
+        {...omit(props, 'children', 'sx')}
+        onChange={onChange ?? (() => {})}
+        sx={{
+          // Material UI is pain, part.. like, eight trillion?
+          '&.MuiSwitch-root .MuiSwitch-switchBase': stylesObject.switchBase,
+          '&.MuiSwitch-root .MuiSwitch-switchBase:hover':
+            stylesObject.switchHover,
+          '&.MuiSwitch-root .MuiSwitch-switchBase.Mui-checked:hover':
+            stylesObject.switchHoverOn,
+          '&.MuiSwitch-root span.MuiSwitch-track':
+            (checked
+              ? stylesObject.switchTrackOn
+              : stylesObject.switchTrackOff) ?? {},
+          '& .MuiButtonBase-root.MuiSwitch-switchBase .MuiSwitch-thumb':
+            stylesObject.switchThumb,
+          '& .MuiButtonBase-root.MuiSwitch-switchBase.Mui-checked .MuiSwitch-thumb':
+            stylesObject.switchThumbOn,
+        }}
+      />
+    </Tooltip>
+  );
+};
+
+export default Switch;
