622aabd34981dd9261aff994ce5b931771e46b4b
Nowaaru <zackyboy135@gmail.com>
2022-02-17 03:28:35 -0700

    accidentally borked my port stuff

--- .erb/scripts/check-port-in-use.js
+++ .erb/scripts/check-port-in-use.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 import detectPort from 'detect-port';
 
-const port = process.env.PORT || '3939';
+const port = process.env.PORT || '4343';
 
 detectPort(port, (err, availablePort) => {
   if (port !== String(availablePort)) {
