ffbe3991834edd8a8177135ab3efcae325a3dd33
Nowaaru <zackyboy135@gmail.com>
2022-02-23 04:05:15 -0700

    Remove unnecessary cache from handler

--- src/main/sources/handler.ts
+++ src/main/sources/handler.ts
@@ -10,11 +10,8 @@ const requireFunc =
 // I actually like the export class filled with no class but actual structures; so we'll use that.
 // I just came back to this comment and I have absolutely *no* clue what that was supposed to mean.
 
-const cachedSources = new Map<string, SourceBase>();
 export default class Handler {
   public static getSource(sourceName: string): SourceBase {
-    if (cachedSources.has(sourceName)) return cachedSources.get(sourceName)!;
-
     const fileSources = window.electron.util.getSourceFiles();
     const fileSourceDirectory = window.electron.util.getSourceDirectory();
 
