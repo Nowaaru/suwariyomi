58b7a4e57d6faf6e2053991c702191f0c7dd1d76
Nowaaru <zackyboy135@gmail.com>
2022-02-26 15:14:04 -0700

    Implement reader settings structure

--- src/main/util/reader.ts
+++ src/main/util/reader.ts
@@ -1,1 +1,19 @@
 // Reader options for individual manga.
+import Enmap from 'enmap';
+
+const readerEnmap = new Enmap<
+  string,
+  { [mangaID: string]: { [overriddenSetting: string]: any } }
+>({
+  name: 'reader',
+});
+
+export default class {
+  static getMangaSettings(mangaID: string) {
+    return readerEnmap.get(mangaID);
+  }
+
+  static set(mangaID: string, overrides: { [setting: string]: any }) {
+    readerEnmap.set(mangaID, overrides);
+  }
+}
